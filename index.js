f9cd5ee06d9d8dcb325fca18051bc9324ca0b689
Code Gaby <webigabriel@gmail.com>
2026-03-22 17:30:26 +0300

    my firstjs

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 
+function calculateTax(amount) {
+    return amount * 0.10;
+}
 
+function convertToUpperCase(text) {
+    return text.toUpperCase();
+}
+
+function findMaximum(num1, num2) {
+    return num1 > num2 ? num1 : num2;
+}
+
+function isPalindrome(word) {
+    let lower = word.toLowerCase();
+    let reversed = lower.split('').reverse().join('');
+    return lower === reversed;
+}
+
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    return originalPrice - (originalPrice * discountPercentage / 100);
+}
 
 
 
